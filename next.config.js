e8684e8ab2074373673b1069b7d3bb6a731199d0
balogun akeem <balogunbiola101@gmail.com>
2023-10-02 13:44:59 +0100

    new

--- next.config.js
+++ next.config.js
@@ -30,12 +30,12 @@ module.exports = {
   env: {
     baseApiUrl: process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000' // development api
-    : 'https://nat-survey.onrender.com/',
+    : 'https://nat-survey.onrender.com',
   },
   publicRuntimeConfig: {
     apiUrl:
     process.env.NODE_ENV === 'development'
     ? 'http://localhost:7000' // development api
-    : 'https://nat-survey.onrender.com/', // production api
+    : 'https://nat-survey.onrender.com', // production api
   },
 }
